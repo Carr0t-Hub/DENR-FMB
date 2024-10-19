f449692be48979e21da9a4495d3eecf7f2373eff
Carr0t-Hub <79178536+Carr0t-Hub@users.noreply.github.com>
2024-10-19 16:03:36 +0800

    Initial Design

---
+++ assets/layouts/navigation.php
@@ -0,0 +1,81 @@
+<header>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="Logo"></a>
+      <div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAbout" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                About
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownAbout">
+                <li><a class="dropdown-item" href="#">What is [E]NGP?</a></li>
+                <li><a class="dropdown-item" href="#">Expected Project Outcome</a></li>
+                <li><a class="dropdown-item" href="#">Strategies</a></li>
+                <li><a class="dropdown-item" href="#">Commodity Roadmap</a></li>
+                <li><a class="dropdown-item" href="#">NGP Logo</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAbout" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Reference
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownAbout">
+                <li><a class="dropdown-item" href="#">Issuances & Legal Bases</a></li>
+                <li><a class="dropdown-item" href="#">Memorandum of Agreement</a></li>
+                <li><a class="dropdown-item" href="#">NSIC Registered Varieties</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAbout" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Accomplishments
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownAbout">
+                <li><a class="dropdown-item" href="#">Statistics</a></li>
+                <li><a class="dropdown-item" href="#">Sites & Contracts</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAbout" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Maps & Photos
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownAbout">
+                <li><a class="dropdown-item" href="#">NGP/ENGP Sites</a></li>
+                <li><a class="dropdown-item" href="#">ERDB Sites</a></li>
+                <li><a class="dropdown-item" href="#">DAR-DENR Sites</a></li>
+                <li><a class="dropdown-item" href="#">Proposed NGP Sites (2023)</a></li>
+                <li><a class="dropdown-item" href="#">NGP Geotagged Photos</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAbout" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Events & Stories
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownAbout">
+                <li><a class="dropdown-item" href="#">Bamboo Month</a></li>
+                <li><a class="dropdown-item" href="#">ENGP Stories</a></li>
+                <li><a class="dropdown-item" href="#">Regional Stories</a></li>
+                <li><a class="dropdown-item" href="#">Regional Gallery</a></li>
+                <li><a class="dropdown-item" href="#">Photo Gallery</a></li>
+                <li><a class="dropdown-item" href="#">Video Gallery</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Contact Us</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <i class="bi bi-search" style="font-size: 20px;"></i>
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+  </header>
